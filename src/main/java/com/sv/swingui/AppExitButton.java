47619cc9759e353fc7286a299e694ac7b8fd6d8e
Shailendra Verma <shailendravermag@gmail.com>
2020-09-27 14:50:15 +0100

    update with parent pom

--- src/main/java/com/sv/swingui/AppExitButton.java
+++ src/main/java/com/sv/swingui/AppExitButton.java
@@ -1,8 +1,25 @@
 package com.sv.swingui;
 
+/**
+ * Created by Shailendra Verma
+ */
 public class AppExitButton extends AppButton {
 
+    /**
+     * Default constructor
+     * exit icon path will be referred from calling application
+     * At root there must be png file with name as <p>exit-icon.png</p>
+     */
     public AppExitButton() {
+        // exit icon need to be
         super("", 'x', "Exit application.", "./exit-icon.png");
+    }
+
+    /**
+     * Boolean value will be ignored and EXIT button will be created
+     */
+    public AppExitButton(boolean noIcon) {
+        // value of noIcon ignored and exit icon will be removed
+        super("Exit", 'x', "Exit application.");
     }
 }
