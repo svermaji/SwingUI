14bf039b8555c3da7a9b49b7074ff823c76b63bc
Shailendra Verma <shailendravermag@gmail.com>
2020-10-16 11:48:23 +0100

    update for progress bar and failed/warning

--- src/main/java/com/sv/swingui/CellRenderer.java
+++ src/main/java/com/sv/swingui/CellRenderer.java
@@ -1,18 +1,33 @@
 package com.sv.swingui;
 
+import com.sv.core.Utils;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
 
 /**
  * Wrapper class for CellRenderer
+ *
+ * If cell value of column 1 (column is fixed) starts with "Failed" or "Cancelled"
+ * it will be marked reds
+ *
+ * Considering:
+ * 0th column will always be used for tooltip
+ * 1st column will be used for indexing
+ * 2nd column will be used for data/naming
  */
 public abstract class CellRenderer extends DefaultTableCellRenderer {
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component c = super.getTableCellRendererComponent(table,value,isSelected,hasFocus,row,column);
+        String name = table.getValueAt(row, 2).toString();
+        System.out.println("name = " + name);
+        boolean isFailedOrCancelled = name.startsWith(Utils.FAILED) ||
+                name.startsWith(Utils.CANCELLED);
+        c.setForeground(isFailedOrCancelled ? Color.RED : Color.BLACK);
         setToolTipText(table.getValueAt(row, 0).toString());
         return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
     }
-
 }
---
+++ src/main/java/com/sv/swingui/CellRendererProgressBar.java
@@ -0,0 +1,31 @@
+package com.sv.swingui;
+
+import com.sv.core.Utils;
+
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+public class CellRendererProgressBar extends JProgressBar
+        implements TableCellRenderer {
+
+    public CellRendererProgressBar() {
+        super(0, 100);
+        setValue(0);
+        setString("0%");
+        setStringPainted(true);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus,
+            int row, int column) {
+
+        setToolTipText(table.getValueAt(row, 0).toString());
+        if (Utils.hasValue(value.toString())) {
+            setValue(Integer.parseInt(value.toString()));
+            setString(value.toString()+"%");
+        }
+        return this;
+    }
+}
