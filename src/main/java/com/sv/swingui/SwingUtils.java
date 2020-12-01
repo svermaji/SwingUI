be82962836a15c954e95986b8900b3b3656fadbf
Shailendra Verma <shailendravermag@gmail.com>
2020-12-01 13:04:40 +0000

    update

--- src/main/java/com/sv/swingui/SwingUtils.java
+++ src/main/java/com/sv/swingui/SwingUtils.java
@@ -181,7 +181,7 @@ public class SwingUtils {
                                       boolean showFonts,
                                       boolean ignoreBlackAndWhite,
                                       Object obj, MyLogger logger) {
-        return getColorsMenu("Colors", 'o', "Colors",
+        return getColorsMenu(showFonts ? "Colors" : "Colors and Fonts", 'o', "Colors",
                 showHighlight, showHighlightFG, showSelected,
                 showFonts, ignoreBlackAndWhite, obj, logger);
     }
