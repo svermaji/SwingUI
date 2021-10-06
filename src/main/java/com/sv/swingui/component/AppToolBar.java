62e2f6ea3526d7ee4794a6f1eed96ea828f43dcf
Shailendra Verma <shailendravermag@gmail.com>
2021-10-06 16:16:32 +0100

    fix of misalign menu in toolbar

---
+++ src/main/java/com/sv/swingui/component/AppToolBar.java
@@ -0,0 +1,21 @@
+package com.sv.swingui.component;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Wrapper class for JToolbar
+ * <p>
+ * When JMenu added to JToolbar it mis-aligns
+ * so manually layout need to be applied.
+ * @link https://stackoverflow.com/questions/69454055/java-swing-menu-control-not-align
+ */
+public class AppToolBar extends JToolBar {
+
+    public static final int gap = 0;
+
+    public AppToolBar() {
+        super();
+        setLayout(new FlowLayout(FlowLayout.LEFT, gap, gap));
+    }
+}
