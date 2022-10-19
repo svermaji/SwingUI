f48b91712f2d8101c2849f259034cf2cbe74f1db
Shailendra Verma <shailendravermag@gmail.com>
2022-10-19 18:51:15 +0100

    update

---
+++ src/main/java/com/sv/swingui/component/AppComboBox.java
@@ -0,0 +1,64 @@
+package com.sv.swingui.component;
+
+import com.sv.core.Utils;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AppComboBox extends JComboBox {
+
+    public AppComboBox(Object[] items, int selectedIdx, String tip) {
+        super(items);
+        if (selectedIdx >= -1 && selectedIdx < items.length) {
+            setSelectedIndex(selectedIdx);
+        }
+        if (Utils.hasValue(tip)) {
+            setToolTipText(tip);
+        }
+    }
+
+    public AppComboBox(Object[] items, Object selectedVal, String tip) {
+        super(items);
+        if (selectedVal != null) {
+            setSelectedItem(selectedVal);
+        }
+        if (Utils.hasValue(tip)) {
+            setToolTipText(tip);
+        }
+    }
+
+    /* For coloring tooltip */
+    private Color fg, bg;
+    private Font tooltipFont;
+
+    public void setToolTipColors(Color fg, Color bg) {
+        this.bg = bg;
+        this.fg = fg;
+    }
+
+    public void setToolTipColorsNFont(Color fg, Color bg, Font f) {
+        this.bg = bg;
+        this.fg = fg;
+        this.tooltipFont = f;
+    }
+
+    public void setTooltipFont(Font tooltipFont) {
+        this.tooltipFont = tooltipFont;
+    }
+
+    @Override
+    public JToolTip createToolTip() {
+        JToolTip tooltip = super.createToolTip();
+        if (bg != null) {
+            tooltip.setBackground(bg);
+        }
+        if (fg != null) {
+            tooltip.setForeground(fg);
+        }
+        if (tooltipFont != null) {
+            tooltip.setFont(tooltipFont);
+        }
+        return tooltip;
+    }
+
+}
